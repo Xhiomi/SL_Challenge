37d54f7e5eb5ba4dca4d43ddffecb4a931ce6f7b
xhiomi <xhiomi@gmail.com>
2022-08-20 15:40:14 -0400

    LWC Datatable created

--- force-app/main/default/lwc/challenge/challenge.js
+++ force-app/main/default/lwc/challenge/challenge.js
@@ -1,3 +1,14 @@
 import { LightningElement } from 'lwc';
 
-export default class Challenge extends LightningElement {}
+const columns = [
+    { label: 'Account Name', fieldName: 'Name', type: 'url' },
+    { label: 'Account Owner', fieldName: 'Owner.Name', type: 'string' },
+    { label: 'Phone', fieldName: 'Phone', type: 'phone' },
+    { label: 'Website', fieldName: 'Website', type: 'url' },
+    { label: 'Annual Revenue', fieldName: 'AnnualRevenue', type: 'currency' },
+];
+
+export default class Challenge extends LightningElement {
+    data = [];
+    columns = columns;
+}
